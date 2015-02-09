6cf92d3ecdc6e63a2932f348fdcf1f2d20845bce
Ville Lahdenvuo <ville.lahdenvuo@luotta.fi>
2015-02-09 20:24:14 +0200

    Remove htaccess and robots.txt

--- gulpfile.js
+++ gulpfile.js
@@ -53,7 +53,6 @@ gulp.task('extras', function () {
   return gulp.src([
     'app/*.*',
     '!app/*.html',
-    'node_modules/apache-server-configs/dist/.htaccess'
   ], {
     dot: true
   }).pipe(gulp.dest('dist'));
